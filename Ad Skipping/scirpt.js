8976083dd02b02ec969cc7e330b5593a1ad4df32
SiTechTech <95187982+SiTechTech@users.noreply.github.com>
2022-06-16 11:47:27 -0400

    Add files via upload

---
+++ Ad Skipping/scirpt.js
@@ -0,0 +1,12 @@
+setInterval(function(){
+
+    var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+    if(skipButton != undefined && skipButton.length > 0){
+        console.log("Ad Was Skipped")
+        skipButton[0].click();
+        
+    }
+}, 3000)
+
+
+
---
+++ popUpSkip/scirpt.js
@@ -0,0 +1,9 @@
+setInterval(function(){
+
+    var skipButton = document.getElementsByClassName("ytp-ad-overlay-close-button");
+    if(skipButton != undefined && skipButton.length > 0){
+        console.log("Pop Up Was Skipped")
+        skipButton[0].click();
+        
+    }
+}, 3000)
